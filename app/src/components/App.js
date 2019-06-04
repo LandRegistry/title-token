4a0d64659a71d3ca7661512915f785872898d8a2
Matt Girdler <matt.girdler@digital.landregistry.gov.uk>
2019-06-04 14:54:08 +0100

    remove redundant css

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import  { Drizzle, generateStore } from "drizzle";
 import { DrizzleContext } from "drizzle-react";
 
-// import "../static/css/App.css";
-
 import drizzleOptions from "../drizzleOptions";
 import TitleTokenApp from "./TitleTokenApp";
 
