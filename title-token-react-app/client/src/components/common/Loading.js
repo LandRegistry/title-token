7a9c4842258aed51bf1689248eaceae6661c0ca5
Matt Girdler <matt.girdler@digital.landregistry.gov.uk>
2019-06-17 15:41:00 +0100

    Add check for owned titles on idv page. Add select title page. Add loading spinner

--- title-token-react-app/client/src/components/common/Loading.js
+++ title-token-react-app/client/src/components/common/Loading.js
@@ -1,21 +1,72 @@
 import React from "react";
-import { H1 } from "@govuk-react/heading";
-import styled from 'styled-components'
 
-const StyledLoading = styled.div`
+import Main from '@govuk-react/main';
+import { H1 } from "@govuk-react/heading";
+
+import styled, { keyframes } from 'styled-components';
+
+const StyledDiv = styled.div`
     text-align: center;
     margin: 2rem;
 `
 
-class Loading extends React.Component {
-    render() {
-        return (
-            <StyledLoading>
-                <H1><span role="img" aria-label="Loading icon">⚙️</span></H1>
-                <p>Loading dapp...</p>
-            </StyledLoading>
-        )
+const animation = keyframes`
+    0% {
+        transform: rotate(0deg);
     }
-}
+    100% {
+        transform: rotate(360deg);
+    }
+`
+
+const Spinner = styled.div`
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+
+    div {
+        box-sizing: border-box;
+        display: block;
+        position: absolute;
+        width: 51px;
+        height: 51px;
+        margin: 6px;
+        border: 6px solid #005ea5;
+        border-radius: 50%;
+        animation: ${animation} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+        border-color: #005ea5 transparent transparent transparent;
+
+        :nth-child(1) {
+            animation-delay: -0.45s;
+        }
+
+        :nth-child(2) {
+            animation-delay: -0.3s;
+        }
+
+        :nth-child(3) {
+        animation-delay: -0.15s;
+        }
+    }
+`
+class Loading extends React.Component {
+
+    render() {
+        let { text } = this.props;
+
+        if (!text) {
+            text = "Loading..."
+        }
+    
+        return(
+            <StyledDiv>
+                <H1>{text}</H1>
+                <Spinner><div></div><div></div><div></div><div></div></Spinner>
+            </StyledDiv>
+        );
+    }
+} 
+
 
 export default Loading;
